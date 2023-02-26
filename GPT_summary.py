f30fd66f5bd7218c5c896ded0789b38ab03c775e
ksyjerry <67769150+ksyjerry@users.noreply.github.com>
2023-02-26 22:03:20 +0900

    Update GPT_summary.py

--- GPT_summary.py
+++ GPT_summary.py
@@ -12,14 +12,14 @@ st.write('Developed by Assurance DA (jae-dong.kim@pwc.com)')
 
 
 
-article_text = st.text_area('요약할 뉴스나 텍스트를 입력해주세요 (참고: 영문은 국문으로 번역되어 제공합니다)', height=300)
-lang = st.radio("언어를 선택해주세요",  ('국문', '영문'))
+article_text = st.text_area('요약할 뉴스나 텍스트를 입력해주세요', height=300)
+lang = st.radio("요약에 사용될 언어를 선택해주세요",  ('국문', '영문'))
 temp = st.slider('요약스타일을 설정하세요 (설정 수치에따라 다양한 결과물을 만나볼 수 있습니다)',0.0,1.0,0.5)
 
 if lang =='국문':
-    text = "이어지는 뉴스를 4개의 요약된 문장으로 변환해주세요: " +article_text
+    text = "Please convert the following sentences into 4 summarized Korean sentences" +article_text
 else:
-    text = "Please convert the following sentences into 4 summarized Korean sentences " +article_text
+    text = "Please convert the following sentences into 4 summarized English sentences " +article_text
 
 if len(article_text) >1600:
     st.warning('좀 더 짧은 텍스트를 입력해주세요')
