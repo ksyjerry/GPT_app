ebf3c7003d74139127ba93652af6e4ab356a6ff4
ksyjerry <67769150+ksyjerry@users.noreply.github.com>
2023-03-05 20:53:21 +0900

    Update pwc_GPT.py

--- pwc_GPT.py
+++ pwc_GPT.py
@@ -45,29 +45,29 @@ user_input = st.text_input('말해보세요', key = 'input')
 st.write(generate_response(user_input))
 
 
-if 'generated' not in st.session_state:
-    st.session_state['generated']=[]
+# if 'generated' not in st.session_state:
+#     st.session_state['generated']=[]
 
-if 'past' not in st.session_state:
-    st.session_state['past']=[]
+# if 'past' not in st.session_state:
+#     st.session_state['past']=[]
 
-def get_text():
-    input_text = st.text_input('안녕하세요! 무엇을 도와드릴까요?' , key ='input')
-    return input_text
+# def get_text():
+#     input_text = st.text_input('안녕하세요! 무엇을 도와드릴까요?' , key ='input')
+#     return input_text
 
-user_input = get_text()
+# user_input = get_text()
 
 
 
-if user_input:
-    output = generate_response(user_input)
-    st.session_state.past.append(user_input)
-    st.session_state.generated.append(output)
+# if user_input:
+#     output = generate_response(user_input)
+#     st.session_state.past.append(user_input)
+#     st.session_state.generated.append(output)
 
-if st.session_state['generated']:
-    for i in range(len(st.session_state['generated'])-1,-1,-1):
-        message(st.session_state['generated'][i],key = str(i))
-        message(st.session_state['past'][i], is_user=True, key = str(i)+'_user')
+# if st.session_state['generated']:
+#     for i in range(len(st.session_state['generated'])-1,-1,-1):
+#         message(st.session_state['generated'][i],key = str(i))
+#         message(st.session_state['past'][i], is_user=True, key = str(i)+'_user')
 
     
 
