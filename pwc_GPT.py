a292bec445f9e8d1450c1c509dc9c1bb9edc3c21
ksyjerry <67769150+ksyjerry@users.noreply.github.com>
2023-03-05 20:51:49 +0900

    Update pwc_GPT.py

--- pwc_GPT.py
+++ pwc_GPT.py
@@ -10,30 +10,30 @@ openai.api_key = st.secrets["api_secret"]
 # gpt-3.5-turbo
 # curie:ft-personal-2023-03-02-00-46-42
 
-# def generate_response(prompt):
-#     completions = openai.Completion.create(
-#         engine = "curie:ft-personal-2023-03-02-00-46-42",
-#         prompt = prompt,
-#         max_tokens = 50, 
-#         n = 1,
-#         stop = None, 
-#         temperature = 0
-#     )
-
-#     message = completions.choices[0].text
-#     return message
-
 def generate_response(prompt):
-    completions = openai.ChatCompletion.create(
-        engine = "gpt-3.5-turbo",
-        messages=[
-        {"role": "system", "content": "당신은 IFRS 회계전문가입니다."},
-        {"role": "user", "content": prompt}
-    ]
+    completions = openai.Completion.create(
+        engine = "curie:ft-personal-2023-03-02-00-46-42",
+        prompt = prompt,
+        max_tokens = 50, 
+        n = 1,
+        stop = None, 
+        temperature = 0
     )
 
-    message = completions.choices[0]['message']['content']
+    message = completions.choices[0].text
     return message
+
+# def generate_response(prompt):
+#     completions = openai.ChatCompletion.create(
+#         engine = "gpt-3.5-turbo",
+#         messages=[
+#         {"role": "system", "content": "당신은 IFRS 회계전문가입니다."},
+#         {"role": "user", "content": prompt}
+#     ]
+#     )
+
+#     message = completions.choices[0]['message']['content']
+#     return message
 
 
 
@@ -41,34 +41,33 @@ st.title('Samil PwC Accounting GPT')
 st.header('IFRS 챗봇')
 st.write('Developed by Assurance DA (jae-dong.kim@pwc.com)')
 
-# user_input = st.text_input('말해보세요', key = 'input')
-# st.write(generate_response(user_input))
+user_input = st.text_input('말해보세요', key = 'input')
+st.write(generate_response(user_input))
 
 
-# if 'generated' not in st.session_state:
-#     st.session_state['generated']=[]
+if 'generated' not in st.session_state:
+    st.session_state['generated']=[]
 
-# if 'past' not in st.session_state:
-#     st.session_state['past']=[]
+if 'past' not in st.session_state:
+    st.session_state['past']=[]
 
 def get_text():
     input_text = st.text_input('안녕하세요! 무엇을 도와드릴까요?' , key ='input')
     return input_text
 
 user_input = get_text()
-output = generate_response(user_input)
-st.write(output)
 
 
-# if user_input:
-#     output = generate_response(user_input)
-#     st.session_state.past.append(user_input)
-#     st.session_state.generated.append(output)
 
-# if st.session_state['generated']:
-#     for i in range(len(st.session_state['generated'])-1,-1,-1):
-#         message(st.session_state['generated'][i],key = str(i))
-#         message(st.session_state['past'][i], is_user=True, key = str(i)+'_user')
+if user_input:
+    output = generate_response(user_input)
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
+
+if st.session_state['generated']:
+    for i in range(len(st.session_state['generated'])-1,-1,-1):
+        message(st.session_state['generated'][i],key = str(i))
+        message(st.session_state['past'][i], is_user=True, key = str(i)+'_user')
 
     
 
